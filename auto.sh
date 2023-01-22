392500716e51f6860574aa839dc838d46154dc05
GoalTV <janusz@mailapp.red>
2023-01-22 18:20:48 +0000

    Update auto.sh
    Optimized

--- auto.sh
+++ auto.sh
@@ -1,8 +1,22 @@
-sudo apt update -y
-sudo apt upgrade -y
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt update -y && sudo apt upgrade -y
+
+# Run Pi-hole update
 sudo pihole -up
+
+# Run whitelist scripts
 sudo sh /root/whitelist/scripts/optional.sh
 sudo sh /root/whitelist/scripts/referral.sh
 sudo sh /root/whitelist/scripts/whitelist.sh
+
+# Regenerate Pi-hole blocklists
 sudo pihole -g
-/sbin/shutdown -r +0
+
+# Check the exit status of the previous command
+if [ $? -eq 0 ]; then
+    # Reboot the system
+    exec /sbin/shutdown -r +0
+else
+    echo "An error occurred.
